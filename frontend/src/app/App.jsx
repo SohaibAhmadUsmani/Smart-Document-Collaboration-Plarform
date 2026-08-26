5da5393772435032a2e1209f32acf4d20931c49b
Shanzay <Shanza23@users.noreply.github.com>
2026-08-26 13:25:06 +0500

    feat(app): wire up react-router-dom, fix main.jsx not rendering App

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { EditorCanvas } from '../modules/editor/index.js';
 
 export function App() {
   return (
     <div className="app-root min-h-screen">
-      <EditorCanvas />
+      <Routes>
+        <Route path="/" element={<EditorCanvas />} />
+        {/* Other teammates and I will add more <Route> entries here as pages are ready */}
+      </Routes>
     </div>
   );
 }
